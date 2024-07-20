6364753de42971a1620953701453a7eec9d4f28e
Michael Cazares <michaelcazares@gmail.com>
2024-07-19 21:30:25 -0700

    first commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace GuessFavoriteNumber
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1001);
+
+            Console.WriteLine("Guess my favorite number between 1 and 1000:");
+
+            while (true)
+            {
+                Console.Write("Enter your guess: ");
+                int userInput;
+                bool isValidInput = int.TryParse(Console.ReadLine(), out userInput);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 1000.");
+                    continue;
+                }
+
+                if (userInput < 1 || userInput > 1000)
+                {
+                    Console.WriteLine("Please enter a number within the range 1 to 1000.");
+                    continue;
+                }
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low.");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high.");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!!!");
+                    break;
+                }
+            }
         }
     }
 }
